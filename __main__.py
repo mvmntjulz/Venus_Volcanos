c9012c08302f42138383e5f2ab9ff389a3579f96
Maximilian Klingmann <max@macskay.com>
2018-03-15 03:34:57 +0100

    50 percent

--- __main__.py
+++ __main__.py
@@ -1,124 +1,118 @@
 import numpy as np
 import os
-import sklearn.decomposition
+import matplotlib.pyplot as plt
+import matplotlib.patches as ptc
 import sklearn.discriminant_analysis
-import sklearn.metrics
-from sklearn.model_selection import train_test_split
-from matplotlib import pylab as plt
 from skimage import exposure
-import scipy.misc
+from skimage.feature import canny, daisy
+from skimage.morphology import reconstruction
+from skimage.restoration import denoise_bilateral, denoise_wavelet, denoise_tv_chambolle
+from sklearn.model_selection import train_test_split
+import skimage.feature
+import sklearn.decomposition
 
-PATCHES = list()
-LABELS = list()
+PATCH_SIZE=15
 
+def load_data(i):
+    image = np.fromfile(os.path.join("data","Images","img{}.sdt".format(i)), dtype=np.uint8) 
+    gt = np.loadtxt(os.path.join("data", "GroundTruths", "img{}.lxyr".format(i)))
+
+    return image, gt
+
+def create_patches(image, coords):
+    coords = coords.T
+    patches = None
+    for x, y in coords.T:
+        x, y = int(x), int(y)
+
+        ypos = y+50-PATCH_SIZE//2
+        xpos = x+50-PATCH_SIZE//2
+        patch = image[ypos:ypos+PATCH_SIZE, xpos:xpos+PATCH_SIZE]
+        #patch_hog, hi = skimage.feature.hog(patch, pixels_per_cell=(10,10), cells_per_block=(5,5), visualise=True)
+        #plt.imshow(hi)
+        #plt.show()
+        flat_patch = patch.ravel()
+        patches = np.vstack([patches, flat_patch]) if patches is not None else flat_patch
+
+    return np.asarray(patches)
 
 def plot(X, y):
-    plt.plot(X[y == '4', 0], X[y == '4', 1], "ko", alpha=0.2)
-    plt.plot(X[y == '3', 0], X[y == '3', 1], "go", alpha=0.4)
-    plt.plot(X[y == '2', 0], X[y == '2', 1], "bo", alpha=0.4)
-    plt.plot(X[y == '1', 0], X[y == '1', 1], "ro", alpha=0.4)
+    plt.plot(X[y == 4, 0], X[y == 4, 1], "ko", alpha=0.2)
+    plt.plot(X[y == 3, 0], X[y == 3, 1], "go", alpha=0.4)
+    plt.plot(X[y == 2, 0], X[y == 2, 1], "bo", alpha=0.4)
+    plt.plot(X[y == 1, 0], X[y == 1, 1], "ro", alpha=0.4)
     plt.show()
 
 
-def pca(X, comp=None):
+if __name__ == "__main__":
+    all_patches = None
+    all_labels = None
+    for i in range(1,134):
+        image, gt = load_data(i)
+        image = np.reshape(image, (1024, 1024))
+        ni = np.zeros((1024 + 2 * 50, 1024 + 2 * 50), dtype="uint8")
+        ni[50:-50, 50:-50] = image
+        image = ni
+        image = exposure.equalize_hist(image)
+        image = denoise_tv_chambolle(image, multichannel=False)
+        image = daisy(image)
 
-    pca = sklearn.decomposition.PCA(n_components='mle' if not comp else comp, svd_solver='full' if not comp else 'auto')
-    pca.fit(X)
+        plt.imshow(image)
+        plt.show()
 
-    return pca.transform(X)
+        #image =
+        #plt.imshow(rec)
+        #plt.show()
 
+        gt = np.atleast_2d(gt)
+        if len(gt) == 0:
+            continue
 
-def pca_knn():
-    trn_X, tst_X, trn_y, tst_y = train_test_split(*load_data(), test_size=0.33, shuffle=True)
-    trn_X_2 = pca(trn_X, comp=2)
+        coords = gt[:,1:3] if len(gt) > 1 else gt[1:3]
+        labels = np.asarray(gt[:,0].T if len(gt) > 1 else gt[0], dtype=np.uint8)
 
-    #plot(trn_X_2, trn_y)
+        patches = create_patches(image, coords)
+        if patches is not np.atleast_2d(patches):
+            continue
 
-    # Training phase
-    knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=3)
-    knn.fit(trn_X_2, trn_y)
+        all_patches = np.vstack([all_patches, patches]) if all_patches is not None else patches
+        all_labels = np.concatenate([all_labels, labels]) if all_labels is not None else labels
 
-    # Testing phase
-    tst_X_2 = pca(tst_X, comp=2)
-    y_pred = knn.predict(tst_X_2)
-
-    print(sklearn.metrics.confusion_matrix(tst_y, y_pred))
-    print(sklearn.metrics.accuracy_score(tst_y, y_pred))
-
-
-def lda():
-    trn_X, tst_X, trn_y, tst_y = train_test_split(*load_data(), test_size=0.33, shuffle=True)
+    X, tst_X, y, tst_y = train_test_split(all_patches, all_labels, test_size=0.33, shuffle=True)
 
     lda = sklearn.discriminant_analysis.LinearDiscriminantAnalysis()
-    lda.fit(trn_X, trn_y)
-    trn_X_2 = lda.transform(trn_X)
-
-    #plot(trn_X_2, trn_y)
+    lda.fit(X, y)
+    X = lda.transform(X)
+    #plot(X, y)
+    #plot(lda.transform(tst_X), tst_y)
 
     y_pred = lda.predict(tst_X)
+    #plot(lda.transform(tst_X), y_pred)
 
-    #knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=5)
-    #knn.fit(trn_X_2, trn_y)
-    #y_pred = knn.predict(lda.transform(tst_X))
+    # pca = sklearn.decomposition.PCA(n_components=2)
+    # pca.fit(X)
+    # X = pca.transform(X)
+    # tst_X = pca.transform(tst_X)
+    # plot(X, y)
+
+    # knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=5)
+    # knn.fit(X, y)
+    #y_pred = knn.predict(tst_X)
+
+
 
     print(sklearn.metrics.confusion_matrix(tst_y, y_pred))
     print(sklearn.metrics.accuracy_score(tst_y, y_pred))
 
-def reshape_data(X, y):
-    rows = X.shape[0]//225
-    X = X.reshape([225, rows]).T
-    y = y.T
-    return X, y
 
 
-def load_data():
-    labels = np.load(os.path.join("data", "Patches", "labels.npy"))
-    patches = np.load(os.path.join("data", "Patches", "patches.npy"))
-    return patches, labels
+
+        
 
 
-def create_patches_for_image(i):
-    patch_size = 50
-    padding_size = 100
-    path_to_image = os.path.join("data", "Images", "img{}.sdt".format(i))
-    path_to_ground_truth = os.path.join("data", "GroundTruths", "img{}.lxyr".format(i))
 
-    # Padding
-    image = np.fromfile(path_to_image, dtype='uint8')
-    image = image.reshape((1024, 1024))
-    new_image = np.zeros((1024 + 2 * padding_size, 1024 + 2 * padding_size), dtype='uint8')
-    new_image[padding_size:-padding_size, padding_size:-padding_size] = image
-    image = new_image
-    #image = exposure.equalize_adapthist(image, clip_limit=0.03)
-
-    with open(path_to_ground_truth) as lines:
-        for line in lines:
-            line_array = line.split(" ")
-            x_pos = int(float(line_array[1])) + 100
-            y_pos = int(float(line_array[2])) + 100
-            patch = image[y_pos:y_pos + patch_size, x_pos:x_pos + patch_size]
-            global LABELS
-            LABELS.append(line[0])
-            global PATCHES
-            PATCHES.append(patch.ravel())
+    
 
 
-def create_patches():
-    for image in range(134):
-        create_patches_for_image(image+1)
-
-    global LABELS
-    LABELS = np.asarray(LABELS)
-    global PATCHES
-    PATCHES = np.asarray(PATCHES)
-
-    np.save(os.path.join("data", "Patches", "patches.npy"), PATCHES)
-    np.save(os.path.join("data", "Patches", "labels.npy"), LABELS)
-
-
-if __name__ == "__main__":
-    #create_patches()
-    #pca_knn()
-    lda()
 
 
